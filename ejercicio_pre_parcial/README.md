8cc5685f39f5144ed607117aa75ce9cbb5e9f0ea
Robert Alberto Caicedo Sanchez <102974396+RobertCaicedo3723@users.noreply.github.com>
2022-05-03 15:00:51 -0500

    Add files via upload

---
+++ ejercicio_pre_parcial/README.md
@@ -0,0 +1,5 @@
+# Ejercicio Pre Parcial
+
+## Programa para determinar si dados 3 numeros enteros la suma de los 2 primeros es igual al tercero
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ ejercicio_pre_parcial/numero_entero.py
@@ -0,0 +1,22 @@
+print("---------------------------------")
+print("--------SUMA DE ENTEROS----------")
+print("---------------------------------")
+
+# Input
+
+X = int (input("Ingrese un numero: " ))
+Y = int (input("Ingrese un numero: " ))
+Z = int (input("Ingrese un numero: " ))
+
+# Output
+
+R = (X + Y)
+
+if R == Z:
+    print("La suma de los dos primeros numeros es: " + str(R))
+
+else:
+    print("La suma de los dos primeros numeros no da el tercer numero ")
+
+
+    
